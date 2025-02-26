0ced454a596d2e7b8c46748b74cef04d5a2fc8fd
Léon Othenin-Girard <lgirard@student.ethz.ch>
2025-02-26 23:29:07 +0100

    changing _layout.tsx for further testing

--- react-testing/app/_layout.tsx
+++ react-testing/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    'Tama-Connect': require('../assets/fonts/tamaconnecttype.ttf'),
+    //'Redensek': require('../assets/fonts/REDENSEK.TTF'),
+    'Press-Start': require('../assets/fonts/PressStart2P-Regular.ttf')
+  });
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
