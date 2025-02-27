0568771c2f37e4e8ad07f68d546f158bd0d8a507
Léon Othenin-Girard <lgirard@student.ethz.ch>
2025-02-27 10:33:54 +0100

    red background test

--- react-testing/app/index.tsx
+++ react-testing/app/index.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { ImageBackground, Text, View, useWindowDimensions, Pressable, Image } from "react-native";
 import { GameStateContextProvider } from "./GameState";
 import { GenerateStyles } from "./styles"; 
-import { ScreenArea } from "./ScreenArea";
-import { GameButtons } from "./GameButtons";
+// import { ScreenArea } from "./ScreenArea";
+// import { GameButtons } from "./GameButtons";
 
 export default function Index() {
   const [gameState, setGameState] = useState(['egg_1']);
@@ -12,23 +12,33 @@ export default function Index() {
   const Styles = GenerateStyles(width, height); 
   console.log("index: " + gameState);
   return (
-    <GameStateContextProvider>
+    // <GameStateContextProvider>
       <View style={Styles.outerView}>
-        <ImageBackground source={{uri: 'assets/images/game-images/tamagotchi-egg.png'}} style={Styles.egg} resizeMode="contain">
-          <ScreenArea Styles={Styles}/>
-          <GameButtons Styles={Styles}/>
-        </ImageBackground>
+        {/* <ImageBackground source={{uri: 'assets/images/game-images/tamagotchi-egg.png'}} style={Styles.egg} resizeMode="contain"> */}
+          {/* <ScreenArea Styles={Styles}/>
+          <GameButtons Styles={Styles}/> */}
+        {/* </ImageBackground> */}
       </View>
-    </GameStateContextProvider>
+    // </GameStateContextProvider>
   );
 }
 
-function tutorialScreen(state: boolean, setState: any, styles: any) {
-  return (
-    <View style={styles.tutorial}>
-    <Pressable>
-      <Image style={styles.tutorialButton} source={{uri: 'assets/images/game-images/options-button.png'}}/>
-    </Pressable>
-    </View>
-  );
-}
+
+// import { Text, View, Image } from "react-native";
+// import { PetImages } from "./PetImages";
+
+// export default function Index() {
+
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//       <Image source={PetImages.pet[0]}/>
+//     </View>
+//   );
+// }
